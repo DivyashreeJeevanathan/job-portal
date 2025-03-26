80b69303d852228455176098160ad8b28b49b575
DivyashreeJeevanathan <127091420+DivyashreeJeevanathan@users.noreply.github.com>
2025-03-26 14:53:19 +0530

    updated signup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
-import RecruiterDashboard from "./pages/recruiter/Dashboard";
-import CandidateBrowseJobs from "./pages/candidate/BrowseJobs";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Router>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/recruiter/dashboard" element={<RecruiterDashboard />} />
-            <Route path="/candidate/browse-jobs" element={<CandidateBrowseJobs />} />
-        </Routes>
+        <App />
     </Router>
 );
